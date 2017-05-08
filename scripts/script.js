0aa22a800fd295cc56ae7f37231e1227a14e3079
Mehul Rana <mehul.r@hcopl.com>
2017-05-08 12:10:37 +0530

    #1 Initialization with concatination, if else, ternary & switch

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+// var myName = "Mehul";
+// var mySname = "Rana";
+//
+// var concat = myName + " " + mySname;
+
+
+
+// var a = 10;
+// var b = 5;
+//
+// if(a < b){
+//   alert("Value of a is less");
+// }
+// else{
+//   alert("Value of b is less");
+// }
+
+
+// var a = 100;
+// var b = 20;
+//
+// var c = (a > b) ? alert("A is greater") : alert("A is less");
+
+var fruit = "banana";
+
+switch (fruit) {
+    case "apple":
+        document.write("Apple")
+        break;
+
+    case "banana":
+        document.write("Banana")
+        break;
+
+    case "orange":
+        document.write("orange")
+        break;
+
+    default:
+        document.write("Out of stock");
+
+}
